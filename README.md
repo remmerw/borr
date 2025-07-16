f22ba9e76853102124aa1e3a783cf5e8c014f29f
remmerw <remmer.wilts@gmail.com>
2025-07-16 17:06:25 +0200

    Version 0.0.3 ------------- Enhance API

--- README.md
+++ README.md
@@ -18,7 +18,7 @@ kotlin {
     sourceSets {
         commonMain.dependencies {
             ...
-            implementation("io.github.remmerw:borr:0.0.2")
+            implementation("io.github.remmerw:borr:0.0.3")
         }
         ...
     }
---
+++ borr/src/commonMain/kotlin/io/github/remmerw/borr/PeerId.kt
@@ -0,0 +1,46 @@
+package io.github.remmerw.borr
+
+
+// hash is always (32 bit) and it is a Ed25519 public key
+data class PeerId(val hash: ByteArray) {
+
+    override fun hashCode(): Int {
+        return hash.contentHashCode() // ok, checked, maybe opt
+    }
+
+    init {
+        require(hash.size == 32) { "hash size must be 32" }
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || this::class != other::class) return false
+
+        other as PeerId
+
+        return hash.contentEquals(other.hash)
+    }
+}
+
+// Note a peerId is always a public key (ed25519)
+@Suppress("ArrayInDataClass")
+data class Keys(val peerId: PeerId, val privateKey: ByteArray)
+
+
+fun generateKeys(): Keys {
+    val keyPair = Ed25519Sign.KeyPair.newKeyPair()
+    return Keys(
+        PeerId(keyPair.getPublicKey()),
+        keyPair.getPrivateKey()
+    )
+}
+
+fun verify(peerId: PeerId, data: ByteArray, signature: ByteArray) { // move to Asen
+    val verifier = Ed25519Verify(peerId.hash)
+    verifier.verify(signature, data)
+}
+
+fun sign(keys: Keys, data: ByteArray): ByteArray { // move to Asen
+    val signer = Ed25519Sign(keys.privateKey)
+    return signer.sign(data)
+}
