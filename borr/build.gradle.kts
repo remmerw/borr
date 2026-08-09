1d48cc696399cc89f7f48f16bc19a1a5373460f8
Remmer Wilts <45624337+remmerw@users.noreply.github.com>
2026-08-09 18:50:21 +0545

    Aktualisieren von build.gradle.kts

--- borr/build.gradle.kts
+++ borr/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.vanniktech.mavenPublish)
+    alias(libs.plugins.ktlint)
 }
 
 group = "io.github.remmerw"
